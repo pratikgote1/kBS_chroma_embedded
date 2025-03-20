8ea8166e3711e32e8323a79b07df9767ee9354c7
pratikgote1 <pgote@deloitte.com>
2025-03-20 17:32:49 +0530

    embedded now working

--- app/services/embedding.py
+++
@@ -1,7 +0,0 @@
-from chromadb.utils import embedding_functions
-
-default_ef = embedding_functions.DefaultEmbeddingFunction()
-
-def get_embedding(text: str):
-    val = default_ef([text])
-    return val
